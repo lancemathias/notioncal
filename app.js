eef1cbe50159e086fe6517d35d53bd4b9724e094
Lance  Math <lmathias@Lances-MacBook-Air.local>
2021-12-27 03:03:21 -0800

    connects to calendar

--- app.js
+++ app.js
@@ -7,67 +7,12 @@ const constantsFile = "constants.json";
 const credsFile = "credentials.json";
 const constants = JSON.parse(fs.readFileSync(constantsFile));
 const creds = JSON.parse(fs.readFileSync(credsFile));
+const token = JSON.parse(fs.readFileSync("token.json"));
 const notion = new Client({ auth: constants.notion_secret });
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-let auth;
 
-// Load client secrets from a local file.
-fs.readFileSync('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Drive API.
-    auth = authorize(JSON.parse(content));
-});
-auth = authorize(creds);
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    // Check if we have previously stored a token.
-    fs.readFileSync("token.json", (err, token) => {
-        if (err) return getAccessToken(oAuth2Client);
-        oAuth2Client.setCredentials(JSON.parse(token));
-    });
-    return oAuth2Client;
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            return oAuth2Client;
-        });
-    });
-}
+const { client_secret, client_id, redirect_uris } = creds.installed;
+const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+oAuth2Client.setCredentials(token);
 
 async function getNewest() {
     try {
@@ -120,13 +65,13 @@ function toCal(events, auth) {
             },
             'end': {
                 'dateTime': addHours(1)
-            }
-            //'color': task.status.select.color
+            },
+            'color': task.status.select.color
         }
         try {
             calendar.events.insert({
                 auth: auth,
-                calendarId: 'primary',
+                calendarId: constants.gc_id,
                 resource: event
 
             })
@@ -138,8 +83,7 @@ function toCal(events, auth) {
 
     const calendar = google.calendar({ version: 'v3', auth });
     events.forEach(element => upload(element));
-    console.log("success");
 }
 
 const events = await getNewest();
-toCal(events, auth);
+toCal(events, oAuth2Client);
